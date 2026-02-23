c7d23d4973768b9e1fae19e28a1452df0d089cb7
zeffyr-000 <sparkman000@hotmail.com>
2026-02-23 21:55:27 +0100

    fix track regression

--- src/app/current/current.component.ts
+++ src/app/current/current.component.ts
@@ -1,5 +1,8 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { TranslocoService, TranslocoPipe } from '@jsverse/transloco';
 import { PlayerService } from '../services/player.service';
+import { SeoService } from '../services/seo.service';
 import { LazyLoadImageDirective } from '../directives/lazy-load-image.directive';
 import {
   NgbDropdown,
@@ -7,8 +10,8 @@ import {
   NgbDropdownMenu,
   NgbDropdownItem,
 } from '@ng-bootstrap/ng-bootstrap';
-import { TranslocoPipe } from '@jsverse/transloco';
 import { QueueStore, AuthStore } from '../store';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-current',
@@ -24,10 +27,23 @@ import { QueueStore, AuthStore } from '../store';
     TranslocoPipe,
   ],
 })
-export class CurrentComponent {
+export class CurrentComponent implements OnInit {
+  private readonly titleService = inject(Title);
+  private readonly metaService = inject(Meta);
+  private readonly seoService = inject(SeoService);
+  private readonly translocoService = inject(TranslocoService);
   readonly playerService = inject(PlayerService);
   readonly queueStore = inject(QueueStore);
   readonly authStore = inject(AuthStore);
+
+  ngOnInit(): void {
+    this.titleService.setTitle(this.translocoService.translate('current_page_title'));
+    this.metaService.updateTag({
+      name: 'description',
+      content: this.translocoService.translate('current_meta_description') || '',
+    });
+    this.seoService.updateCanonicalUrl(`${environment.URL_BASE}current`);
+  }
 
   play(index: number, isInitialIndex: boolean) {
     this.playerService.lecture(index, isInitialIndex);
