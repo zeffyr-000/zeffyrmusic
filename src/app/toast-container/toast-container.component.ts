2890a78d320442fdaa1eba10e68655d5f3e22426
zeffyr-000 <sparkman000@hotmail.com>
2026-03-07 22:04:18 +0100

    init app with erros

--- src/app/toast-container/toast-container.component.ts
+++ src/app/toast-container/toast-container.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, PLATFORM_ID } from '@angular/core';
 import { NgbToast } from '@ng-bootstrap/ng-bootstrap';
 
 import { UiStore } from '../store/ui/ui.store';
@@ -15,6 +16,9 @@ import { Notification } from '../store/ui/ui.models';
 })
 export class ToastContainerComponent {
   private readonly uiStore = inject(UiStore);
+  private readonly platformId = inject(PLATFORM_ID);
+
+  readonly isBrowser = isPlatformBrowser(this.platformId);
 
   readonly notifications = this.uiStore.notifications;
 
